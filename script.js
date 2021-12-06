4f087c6d1d7a43a699f87d320b251c46c0b8300d
My Name <passivevoice3@gmail.com>
2021-12-07 01:34:26 +0300

    feat: code refactoring and add anti lar13chest security

--- script.js
+++ script.js
@@ -6,117 +6,136 @@ const commissar = 1;
 let players = 0;
 let mafias = 0;
 
-function generateRoles() {
+// it's fantastic
+const roleGenerator = (players) => (...functions) => functions.reduce((previousValue, currentFunction) => currentFunction(previousValue), players);
+
+const removeAllChildNodes = (parent) => {
+    while (parent.firstChild) parent.removeChild(parent.firstChild);
+}
+
+const generatePlayers = (players) => {
+    const arr = [];
+
+    for (let i = 1; i <= players; i++) {
+        arr.push(i);
+    }
+
+    return arr;
+};
+
+const setRole = (arr) => {
+    const result = arr.map((elem, index, arr) => {
+
+    if(index === 0) return elem = {commissar: elem};
+    if(index === 1) return elem = {godfather: elem};
+    // add 3 mafias if more 9 players and mafias variable equal 3
+    if(mafias > 2 && arr.length > 8) {
+        if (index === 2) return elem = {mafia: elem};
+        if (index === 3) return elem = {mafia: elem};
+    } else {
+        if (index === 2) return elem = {mafia: elem};
+    }
+    
+    return {citizen: elem};
+    });
+
+    return result;
+}
+
+const shaker = (arr) => {
+    arr.sort(() => 0.5 - Math.random());
+    
+    return arr;
+};
+
+const generateRoles = () => {
     if(players === 0 || mafias === 0) return;
     
     if(resultBlock.childNodes.length > 1) removeAllChildNodes(resultBlock);
 
-    const allPlayers = [];
-    const activeRole = getRandom(players);
-    
-    for (let i = 1; i < players + commissar; i++) {
-        allPlayers.push(i);        
-    }
-
-    const citizens = allPlayers.filter(e => activeRole.indexOf(e) < 0);
+    const displayRoles = roleGenerator(players)(generatePlayers,shaker,setRole);
     
     const createCitizenTitle = document.createElement('p');
+
     createCitizenTitle.textContent = 'Citizens';
     createCitizenTitle.classList.add('result-block__title');
     resultBlock.append(createCitizenTitle);
 
-    const createCitizenList = document.createElement('ul');
-    createCitizenList.classList.add('result-block__list');
-    
-    citizens.forEach( e => {
-        let createLi = document.createElement('li');
-        createLi.classList.add('result-block__item');
-        createLi.textContent = `${e} - citizen`;
-        createCitizenList.append(createLi);
-    });
+    const citizenList = document.createElement('ul');
 
-    resultBlock.append(createCitizenList);
+    citizenList.classList.add('result-block__list');
 
-    // active role 
+    resultBlock.append(citizenList);
+
     const activeCitizenTitle = document.createElement('p');
+
     activeCitizenTitle.textContent = 'Active';
     activeCitizenTitle.classList.add('result-block__title');
     resultBlock.append(activeCitizenTitle);
     
     const activeCitizenList = document.createElement('ul');
-    activeCitizenList.classList.add('result-block__list-active');
 
-    activeRole.forEach((e, index) => {
+    activeCitizenList.classList.add('result-block__list-active');
+    
+    displayRoles.map((e, index, arr) => {
         let createLi = document.createElement('li');
         createLi.classList.add('result-block__item');
-        
+
         switch (index) {
             case 0:
-                createLi.textContent = `${e} - commissar`;
+                createLi.textContent = `${e.commissar} - commissar`;
+                activeCitizenList.append(createLi);
+
                 break;
             case 1:
-                createLi.textContent = `${e} - godfather`;
+                createLi.textContent = `${e.godfather} - godfather`;
+                activeCitizenList.append(createLi);
+
                 break;
             case 2:
-                createLi.textContent = `${e} - mafia`;
+                createLi.textContent = `${e.mafia} - mafia`;
+                activeCitizenList.append(createLi);
+
                 break;
             case 3:
-                if(activeRole.length > 3) {
-                    createLi.textContent = `${e} - mafia`;
+                if (mafias > 2 && arr.length > 8) {
+                    createLi.textContent = `${e.mafia} - mafia`,
+                    activeCitizenList.append(createLi);
                 } else {
-                    break;
+                    createLi.textContent = `${e.citizen} - citizen`;
+                    citizenList.append(createLi);
                 }
+                
+                break;
 
             default:
+                
                 break;
         }
 
-        activeCitizenList.append(createLi);
+        if(index > 3) {
+            createLi.textContent = `${e.citizen} - citizen`;
+            citizenList.append(createLi);
+        }
     })
 
     resultBlock.append(activeCitizenList);
-}
-
-function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-}
-
-function getRandom(max) {
-    const min = 1;
-    let length = mafias + commissar;
-    const activeRole = []
-
-    // generate unique numbers
-    for (let i = 0; i < length; i++) {
-        let getRandomNumber = Math.floor(Math.random() * ( (max + 0.5) - (min - 0.5))) + min;
-
-        if (activeRole.includes(getRandomNumber)) {
-            // console.log('duplicate ' + getRandomNumber);
-            // add +1 to loop and re-random number again
-            length++;
-        } else {
-            //add unique number
-            // console.log('new ' + getRandomNumber);
-            activeRole.push(getRandomNumber);
-        }
-    }
-
-    return activeRole;
-}
+};
 
 numberOfPlayers.addEventListener('blur', e => {
     let et = parseInt(e.target.value);
     
-    if(et < 5 || et > 11) {
-        // alert('Enter 6 to 10 players');
+    if(et < 6 || et > 12) {
         numberOfPlayers.value = '';
         return;
     }
-    
+
+    if(e.target.value.length > 2) {
+        numberOfPlayers.value = '';
+        return;
+    }
+
     if(isNaN(et)) {
-        // alert('Enter 6 to 10 players');
         numberOfPlayers.value = '';
         return;
     }
@@ -131,6 +150,11 @@ numberOfMafias.addEventListener('blur', e => {
         return;
     }
     
+    if(e.target.value.length > 1) {
+        numberOfMafias.value = '';
+        return;
+    }
+
     if(isNaN(et)) {
         numberOfMafias.value = '';
         return;
